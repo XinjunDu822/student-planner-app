6b55295993e8b2f8b46341073a96905c1bc0ceb5
RyanWong08880 <paradox08880@gmail.com>
2025-11-15 02:03:01 -0800

    Replaced skeleton code with restoration of sign up and sign in implementations

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,78 @@
-export const signIn = (req, res) => {
-  res.send("signIn working");
+import { Request, Response, NextFunction } from "express";
+import prisma from "../prisma.ts";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const JWT_SECRET = process.env.JWT_SECRET || "supersecret"; // store in env in real projects
+
+type Payload = {
+  id: string;
+  email: string;
 };
 
-export const signUp = (req, res) => {
-  res.send("signUp working");
+const hashPassword = async (password: string) => {
+  return await bcrypt.hash(password, 10);
+};
+
+const comparePassword = async (password: string, hash: string) => {
+  return await bcrypt.compare(password, hash);
+};
+
+export const createToken = (payload: Payload): string => {
+  return jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: "1h",
+  });
+};
+
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await hashPassword(password);
+
+    const user = await prisma.user.create({
+      //creates new account for user
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const token = createToken({ id: user.id, email: user.email }); //create token for user after signing in
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "User Email does not exist" });
+    }
+
+    const isValid = comparePassword(password, user.password);
+
+    if (!isValid) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    const token = createToken({ id: user.id, email: user.email });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
