3fc3718f1c65f6562b3ffd49c5e8d971b212c8e9
marvin8tran <marvin8tran@gmail.com>
2025-11-14 22:58:07 -0800

    updated prisma Task and User DB Schema

---
+++ backend/prisma/migrations/20251115065657/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `description` to the `Task` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Task` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" DATETIME NOT NULL,
+    "isComplete" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("deadline", "id", "title") SELECT "deadline", "id", "title" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
