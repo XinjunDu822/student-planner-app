2b81a5e9f8fcb85c7792ef15b642acc39ababfab
marvin8tran <marvin8tran@gmail.com>
2025-11-21 16:16:00 -0800

    fixed auth routing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,14 +6,21 @@ import {
   updateTask,
   deleteTask,
 } from "./controllers/taskController.ts";
+import { signIn, signUp } from "./controllers/authController.ts";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//task routes
 app.use("/api", getAllTasks);
 app.use("/api", createTask);
 app.use("/api", updateTask);
 app.use("/api", deleteTask);
+
+//auth routes
+app.use("/api", signIn);
+app.use("/api", signUp);
 
 export default app;
