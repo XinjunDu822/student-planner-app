d6c4770db7f61bf142078e2e6684f15db89822ea
RyanWong08880 <paradox08880@gmail.com>
2025-11-23 01:07:50 -0800

    commented out the testing stuff

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,10 +12,10 @@ app.use(express.json());
 //app.use("/api", taskRouter);
 app.use("/api/auth", authRouter);
 
-//temp
-app.get("/test/protected", authMiddleware, (req, res) => {
-  res.status(200).json({ message: `Hello ${req.user?.name || "Guest"}` });
-});
+//temp for testing auth middleware
+//app.get("/test/protected", authMiddleware, (req, res) => {
+//  res.status(200).json({ message: `Hello ${req.user?.name || "Guest"}` });
+//});
 
 
 export default app;
