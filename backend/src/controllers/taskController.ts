3a7a6f5212bdbe7da59167dbd5a2b6e961372761
marvin8tran <marvin8tran@gmail.com>
2025-11-22 16:46:06 -0800

    clean yo code bruh

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -8,34 +8,32 @@ export const getAllTasks = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => 
-{
+) => {
   try {
-    const allTasks = await prisma.task.findMany({orderBy: {deadline: 'asc'}});
+    const allTasks = await prisma.task.findMany({
+      orderBy: { deadline: "asc" },
+    });
     // if (!allTasks)
     // {
     //   res.status(400).json({ message: "User does not have any tasks at the moment" });
     // }
     res.json(allTasks);
-  }
-  
-  catch (err) {
+  } catch (err) {
     res.status(500).json({ message: "Server Error" });
   }
-
 };
 
 export const createTask = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => 
-{
+) => {
   try {
-    const { title, deadline, description} = req.body;
-    if (!title || !deadline || !description)
-    {
-      return res.status(400).json({ message: "Please enter a title, description and deadline" });
+    const { title, deadline, description } = req.body;
+    if (!title || !deadline || !description) {
+      return res
+        .status(400)
+        .json({ message: "Please enter a title, description and deadline" });
     }
     const task = await prisma.task.create({
       data: {
@@ -43,11 +41,9 @@ export const createTask = async (
         deadline: new Date(deadline),
         description,
       },
-    }
-  );
-  res.json(task);
-  }
-  catch (err) {
+    });
+    res.json(task);
+  } catch (err) {
     res.status(500).json({ message: "Server Error" });
   }
 };
@@ -56,11 +52,10 @@ export const updateTask = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => 
-{
-    try {
+) => {
+  try {
     const taskId = req.params.id;
-    const { title, deadline, description} = req.body; // add more fields later
+    const { title, deadline, description } = req.body; // add more fields later
 
     const updatedTask = await prisma.task.update({
       where: { id: taskId },
@@ -81,16 +76,15 @@ export const deleteTask = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => 
-{
-try {
+) => {
+  try {
     const taskId = req.params.id;
     const { title, deadline } = req.body; // add more fields later
 
     const deletedTask = await prisma.task.delete({
       where: { id: taskId },
     });
-    res.json({ message: "Task deleted", deletedTask});
+    res.json({ message: "Task deleted", deletedTask });
   } catch (err) {
     res.status(404).json({ message: "Cannot find task" });
   }
