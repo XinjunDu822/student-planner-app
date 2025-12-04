412de1ff3300a1b632008caeba97d1793aa983b3
marvin8tran <marvin8tran@gmail.com>
2025-12-04 13:57:29 -0800

    fixed taskRoutes

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -10,9 +10,14 @@ import { authMiddleware } from "../middleware/authMiddleware.ts";
 const router = express.Router();
 
 //i commented out authentication for now - marvin
+//all our routes, requires JWT Token with authMiddleware
 router.get("/dashboard", authMiddleware, getTaskData);
 router.post("/task", authMiddleware, createTask);
 router.put("/task/:taskId", authMiddleware, updateTask);
 router.delete("/task/:taskId", authMiddleware, deleteTask);
+
+//merged into getTaskData, one general endpoint
+//router.put("/late/", authMiddleware, updateLastLate);
+//router.put("/streak/", authMiddleware, updateBestStreak);
 
 export default router;
